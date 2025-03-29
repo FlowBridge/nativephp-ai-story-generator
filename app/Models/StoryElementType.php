10fe343ee6d1c6cef8913f5976a1f6e720a4190b
Michael Guimaraes <michael1994@live.nl>
2025-03-30 00:28:40 +0200

    returning URL if is S3

--- app/Models/StoryElementType.php
+++ app/Models/StoryElementType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Storage;
 
 class StoryElementType extends Model
 {
@@ -12,5 +13,11 @@ class StoryElementType extends Model
     public function elements(): HasMany
     {
         return $this->hasMany(StoryElement::class);
+    }
+
+    public function getImageAttribute(): string
+    {
+
+        return config('filesystems.default') === 's3' ? Storage::url($this->image) : $this->image;
     }
 }
